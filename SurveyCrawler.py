7acd0f1d164a32e2201f71398d8edd8d686457aa
Fleischman-Soffera <Kai.Fleischman-Soffera@coxautoinc.com>
2018-06-26 13:35:15 -0500

    Added more helpful console messages

--- SurveyCrawler.py
+++ SurveyCrawler.py
@@ -1,4 +1,5 @@
 import time
+import sys
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from RatingScraper import ScrapeRatings
@@ -12,13 +13,20 @@ def CrawlSurveys(driver, department):
             survey_text = survey.text.split(' ')
             if " " in department:
                 survey_department = (survey_text[len(survey_text) - 5]) + " " + survey_text[len(survey_text) - 4][:1]
+                class_name = survey_text[len(survey_text) - 4][:1]
             elif len(department) is 2:
                 survey_department = (survey_text[len(survey_text) - 5])
+                class_name = survey_text[len(survey_text) - 4]
             else: 
                 survey_department = (survey_text[len(survey_text) - 4])[:3]                     # Get department name
+                class_name = survey_text[len(survey_text) - 4][3:]
             if survey_department != department:
                 department_finished = True
                 break
+            sys.stdout.write("\r                                ")
+            sys.stdout.flush()
+            sys.stdout.write("\rCurrent course: " + department + ", " + class_name)
+            sys.stdout.flush()
             survey_link = survey.find_elements_by_tag_name('td')[0]                             # Element containing link
             time.sleep(2)
             survey_link.find_elements_by_tag_name('a')[0].click()                               # Click actual link
